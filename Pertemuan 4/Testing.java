b5a8c91e52c0adfb9a0ba4090ab4fe6d57f4cec8
xfrih <152276740+xfrih@users.noreply.github.com>
2024-09-26 14:20:31 +0700

    Update Testing.java

--- Pertemuan 4/Testing.java
+++ Pertemuan 4/Testing.java
@@ -1,54 +1,39 @@
-import java.util.Scanner;
+/**
+ * Write a description of class TicketMachine here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Operasi {
+    private float num1;
+    private float num2;
 
-public class Testing
-{
-   public static void main(String[]args){
-        Scanner input = new Scanner(System.in);
-        Kalkulator kalkulator = new Kalkulator();
+    public void setNumber1(float amount) {
+        num1 = amount;
+    }
 
-        System.out.println("------------------------");
-        System.out.println("Kalkulator");
-        System.out.println("------------------------");
+    public void setNumber2(float amount) {
+        num2 = amount;
+    }
 
-        //input angka yang kan digunakan 
-        System.out.println("Masukkan angka pertama: ");
-        double num1 = input.nextDouble();
-        kalkulator.isiNum1(num1); // Set num1 di Kalkulator
-        System.out.println("Masukkan angka kedua: ");
-        double num2 = input.nextDouble();
-        kalkulator.isiNum2(num2); // Set num2 di Kalkulator
+    public float penjumlahan() {
+        return num1 + num2;
+    }
 
-        System.out.println("------Jenis Operasi------");
-        System.out.println("1. Penjumlahan");
-        System.out.println("2. Pengurangan");
-        System.out.println("3. Perkalian");
-        System.out.println("4. Pembagian");
-        System.out.println("Pilih Operasi: ");
-        int pilih = input.nextInt();
-        System.out.println("-----------Hasil----------");
-            
-        switch (pilih) {
-            case 1:
-                System.out.println("Hasil dari: " + num1 + " + " + num2 + " = " + kalkulator.tambah());
-                break;
-            case 2:
-                System.out.println("Hasil dari: " + num1 + " - " + num2 + " = " + kalkulator.kurang());
-                break;
-            case 3:
-                System.out.println("Hasil dari: " + num1 + " * " + num2 + " = " + kalkulator.kali());
-                break;
-            case 4:
-                if (num2 != 0) {
-                    System.out.println("Hasil dari: " + num1 + " / " + num2 + " = " + kalkulator.bagi());
-                } else {
-                    System.out.println("Pembagian dengan nol tidak valid.");
-                }                
-                break;
-            default:
-                System.out.println("Operasi tidak valid");
-                break;
-       }
-       input.close();
-       System.out.println("---------------------------");
-   }
+    public float pengurangan() {
+        return num1 - num2;
+    }
+
+    public float perkalian() {
+        return num1 * num2;
+    }
+
+    public float pembagian() {
+        if(num2 != 0){
+            return num1 / num2;
+        }else{
+            System.out.println("Error!");
+            return 0;
+        }
+    }
 }
