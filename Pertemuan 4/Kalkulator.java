23e65e084784730651676204b228b1ceba081f53
xfrih <152276740+xfrih@users.noreply.github.com>
2024-09-26 14:19:38 +0700

    Update Kalkulator.java

--- Pertemuan 4/Kalkulator.java
+++ Pertemuan 4/Kalkulator.java
@@ -1,32 +1,31 @@
-public class Kalkulator
-{
-    private double num1;
-    private double num2;
+import java.util.Scanner;
 
-    //mengisi nilai num1
-    public void isiNum1 (double x){
-        num1 = x;
-    }
-    
-    //mengisi nilai num2
-    public void isiNum2 (double y) {
-        num2 = y;
-    }
-    
-    // Method operasi menggunakan variabel instance num1 dan num2
-    public double tambah(){
-        return num1 + num2;
-    }
-    
-    public double kurang(){
-        return num1 - num2;
-    }
-    
-    public double kali(){
-        return num1 * num2;
-    }
-    
-    public double bagi(){
-        return num1 / num2;
+public class Kalkulator {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        Operasi operasi = new Operasi();
+
+        System.out.print("Masukkan angka pertama: ");
+        float number1 = scanner.nextFloat();
+        operasi.setNumber1(number1);
+
+        System.out.print("Masukkan angka kedua: ");
+        float number2 = scanner.nextFloat();
+        operasi.setNumber2(number2);
+
+        System.out.print("Operasi (1: Penjumlahan, 2: Pengurangan, 3: Perkalian, 4: Pembagian): ");
+        int pilihan = scanner.nextInt();
+
+        if(pilihan == 1){
+            System.out.println("Hasil: " + operasi.penjumlahan());
+        }else if (pilihan == 2){
+            System.out.println("Hasil: " + operasi.pengurangan());
+        }else if (pilihan == 3){
+            System.out.println("Hasil: " + operasi.perkalian());
+        }else if (pilihan == 4){
+            System.out.println("Hasil: " + operasi.pembagian());
+        }else{
+            System.out.println("Error!");
+        }
     }
 }
